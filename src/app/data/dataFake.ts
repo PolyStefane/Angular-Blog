3597fba90610762bc63a824ebcbc44ead2987488
PolyStefane <polyvstefane@gmail.com>
2024-02-22 14:41:12 -0300

    content criado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Palworld (o 'Pokémon de tiro') é lançado e recebe críticas e elogios pelo mundo todo",
+        "description": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A quae dolorum eum alias consequuntur id ratione aperiam cum soluta! Voluptatum, dicta! Tenetur veritatis nemo repellendus, deleniti natus nam optio voluptatem! Lorem, ipsum dolor sit amet consectetur adipisicing elit. A quae dolorum eum alias consequuntur id ratione aperiam cum soluta! Voluptatum, dicta! Tenetur veritatis nemo repellendus, deleniti natus nam optio voluptatem! Lorem, ipsum dolor sit amet consectetur adipisicing elit. A quae dolorum eum alias consequuntur id ratione aperiam cum soluta! Voluptatum, dicta! Tenetur veritatis nemo repellendus, deleniti natus nam optio voluptatem! Voluptatum, dicta! Tenetur veritatis nemo repellendus, deleniti natus nam optio voluptatem! Lorem, ipsum dolor sit amet consectetur adipisicing elit. A quae dolorum eum alias consequuntur id ratione aperiam cum soluta! Voluptatum, dicta! Tenetur veritatis nemo repellendus, deleniti natus nam optio voluptatem! Lorem, ipsum dolor sit amet consectetur adipisicing elit. A quae dolorum eum alias consequuntur id ratione aperiam cum soluta! Voluptatum, dicta! Tenetur veritatis nemo repellendus, deleniti natus nam optio voluptatem!",
+        "photoCover": "https://cdn.akamai.steamstatic.com/steam/apps/1623730/capsule_616x353.jpg?t=1707904340"
+
+    },
+
+
+    {
+        "id": "2",
+        "title": "PlayStation 5: Aproveite a Black Friday e tenha até R$ 1000 de desconto no console mais amado do mundo gamer",
+        "description": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A quae dolorum eum alias consequuntur id ratione aperiam cum soluta! Voluptatum, dicta! Tenetur veritatis nemo repellendus, deleniti natus nam optio voluptatem! Lorem, ipsum dolor sit amet consectetur adipisicing elit. A quae dolorum eum alias consequuntur id ratione aperiam cum soluta! Voluptatum, dicta! Tenetur veritatis nemo repellendus, deleniti natus nam optio voluptatem!",
+        "photoCover": "https://t.ctcdn.com.br/nGVJIIDeNKTRcsK8F5bFRcjv7IA=/1920x1080/smart/i780156.jpeg"
+
+    },
+
+    {
+        "id": "3",
+        "title": "Elden Ring: Shadow of the Erdtree DLC Trailer revela novos inimigos, data de lançamento e muito mais",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vestibulum nibh est, ac tempus metus fringilla in. Nam ut mi mattis, viverra est at, porta justo. Aliquam fringilla nulla magna, a feugiat libero vulputate dictum. Vivamus vulputate placerat maximus. Maecenas viverra leo dui, ac vehicula eros venenatis quis. Aliquam nec odio augue. Ut imperdiet fringilla magna, eget interdum leo interdum nec. Pellentesque ac leo nunc. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc vitae dolor est. Nullam vel odio lorem. Phasellus justo velit, posuere in felis at, posuere suscipit lorem. Vivamus at elementum leo. Phasellus ipsum ipsum, fringilla id lectus ac, lobortis dignissim leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum interdum, nulla non imperdiet euismod, lectus diam dignissim neque, ut accumsan sem velit at velit. Pellentesque pharetra eget arcu et rutrum. Vivamus vitae elit placerat, ultrices sapien vitae, scelerisque massa. Maecenas lectus arcu, efficitur eu dui et, semper mattis sem. In a enim scelerisque, ultricies eros quis, venenatis est. Donec porttitor odio eget vestibulum dapibus. Nulla metus ligula, rhoncus vitae consectetur at, ultrices ut enim. Praesent faucibus erat quis dolor porttitor, vitae dignissim felis blandit. Ut tincidunt non dolor eget placerat.",
+        "photoCover" : "https://uploads.adrenaline.com.br/2024/02/elden-ring-shadow-of-the-erdtree-2-1200x675.jpg"
+    },
+
+    {
+        "id": "4",
+        "title": "Todos os principais lançamentos de videogame em breve para Nintendo Switch",
+        "description": "Pellentesque mollis eros nec nisi facilisis pretium. Duis pellentesque a felis nec aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et interdum justo. Sed iaculis sapien vel semper consectetur. Nulla hendrerit pellentesque purus sit amet sagittis. Vestibulum aliquet imperdiet felis, sed rutrum augue semper id. Pellentesque libero lacus, molestie vel lacus gravida, egestas tristique nibh. Etiam nec venenatis ipsum. Pellentesque at vehicula eros, sit amet tincidunt lorem. Duis quis finibus tortor, a aliquet turpis. Nunc neque urna, mattis eget nibh quis, venenatis pretium felis. Duis congue elit sit amet lectus tincidunt egestas.Nullam mi turpis, cursus et laoreet hendrerit, elementum ut elit. Duis eget libero ligula. Fusce felis ligula, tincidunt eget auctor vel, hendrerit id augue. Praesent tincidunt urna pharetra, luctus ligula non, mollis lacus. Sed vestibulum gravida lorem, ut gravida tortor dapibus vel. Cras tincidunt, orci tempor sagittis interdum, ex mauris viverra urna, et condimentum libero velit a dui. Maecenas sit amet lectus nec odio feugiat consequat at vel nulla. Praesent vestibulum nulla in velit auctor pulvinar. Proin eu augue hendrerit, vulputate ipsum a, mollis felis. Nulla volutpat vel magna a semper.",
+        "photoCover" : "https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/software/switch/70010000005022/cd94b037b282fc3415f980bb31e41de61200235dfc7b1532ffc512280fed0bb1"
+    }
+] 
